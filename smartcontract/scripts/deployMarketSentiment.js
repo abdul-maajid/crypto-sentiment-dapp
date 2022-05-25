20aac4fda96e129dd4b9dafb744b0792a5b6e16f
Abdul Maajid <amaajid0@gmail.com>
2022-05-25 14:44:01 +0500

    contract added

---
+++ smartcontract/scripts/deployMarketSentiment.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+const hreEthscan = require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const MarketSentiment = await hre.ethers.getContractFactory(
+    "MarketSentiment"
+  );
+  const marketsentiment = await MarketSentiment.deploy("Hello, Hardhat!");
+
+  await marketsentiment.deployed();
+
+  console.log("MarketSentiment deployed to:", marketsentiment.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
